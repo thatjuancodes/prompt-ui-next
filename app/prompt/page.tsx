b452e40cb634c0adeb48f936749c84c946f49c0d
thatjuancodes <thejuan.codes@gmail.com>
2024-09-24 00:45:43 +0700

    improve submission function

--- app/prompt/page.tsx
+++ app/prompt/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import PrimaryButton from "../components/PrimaryButton";
 import TextInput from "../components/TextInput";
 import styles from "../page.module.css";
@@ -10,9 +10,10 @@ import { mock } from "node:test";
 export default function Prompt() {
   const [isSubmitted, setIsSubmitted] = useState(false)
   const [userInput, setUserInput] = useState('')
-  const [mockResponse, setMockResponse] = useState<Array<string>>([])
+  const [mockResponse, setMockResponse] = useState<Array<string | Element | React.JSX.Element>>([])
+  const responseEndRef = useRef<HTMLDivElement | null>(null);
 
-  const generateMockResponse = (textInput) => {
+  const generateMockResponse = (textInput: string) => {
     const mockResponse = [
       "I'm sorry, I don't understand.",
       "I'm not sure what you're asking.",
@@ -32,20 +33,27 @@ export default function Prompt() {
 
   const handleSubmit = () => {
     setMockResponse(prevMockResponse => [...prevMockResponse, generateMockResponse(userInput)])
-
     setIsSubmitted(true)
+    setUserInput('')
+    setTimeout(() => {
+      if (responseEndRef.current === null) return
+
+      responseEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }, 100);
   }
 
   return (
-    <div className={`${styles.page} border border-red-500 h-screen`}>
+    <div className={`${styles.page} border h-screen`}>
       <h1 className="text-3xl font-bold">Prompt UI</h1>
 
-      <div className={`mt-5 pl-5 border ${isSubmitted ? 'h-3/5' : ''} border-red-500`} style={{ overflowY: 'scroll' }}>
+      <div className={`mt-5 pl-5 ${isSubmitted ? 'h-3/5 pb-5' : ''}`} style={{ overflowY: 'scroll' }}>
         {mockResponse.map((response, index) => (
           <div key={index} className="mt-5 pl-5">
-            <p className="text-lg">{response}</p>
+            {response}
           </div>
         ))}
+
+        <div ref={responseEndRef} />
       </div>
 
       <div className={`transition-transform duration-1000 ease-in-out fixed left-16 right-16 ${isSubmitted ? `transform ${styles.translateY2full} bottom-0` : 'transform translate-y-0'}`}>
